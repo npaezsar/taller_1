a465da8fd0f00336844ee325b888255d2843a9e1
npaezsar <npaezsar@correo.uis.edu.co>
2022-04-17 19:01:56 -0500

    Estructura básica del taller

---
+++ ejercicio_1/iva.py
@@ -0,0 +1,14 @@
+"""PROGRAMA PARA CALCULAR EL IVA Y 
+ PRECIO DE VENTA DE UN PRODUCTO"""
+
+print("-----------------------------------")
+print("---- IVA Y PRECIO DE VENTA --------")
+print("-----------------------------------")
+
+# input
+
+
+# processing
+
+
+# output
